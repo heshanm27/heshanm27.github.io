695f7160bb20cbdb775523597dc29918bf7d36b0
B.D.H Madhuranga <39455525+heshanm27@users.noreply.github.com>
2021-07-23 16:36:28 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,19 @@
+
+$(document).ready(function(){
+
+    $('.sidenav').sidenav();
+    
+    });
+
+
+    document.addEventListener('DOMContentLoaded', function() {
+        var elems = document.querySelectorAll('.parallax');
+        var instances = M.Parallax.init(elems, options);
+      });
+    
+      // Or with jQuery
+    
+      $(document).ready(function(){
+        $('.parallax').parallax();
+      });
+         
